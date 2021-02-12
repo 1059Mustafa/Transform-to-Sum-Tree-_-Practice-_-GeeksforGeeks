a225f840c0cfeb453566860d00c96bbf523acb19
1059Mustafa <72117500+1059Mustafa@users.noreply.github.com>
2021-02-12 16:27:12 +0530

    Add files via upload

---
+++ binary_21.cpp
@@ -0,0 +1,17 @@
+int f(Node* node)
+{
+    if(!node)
+    return 0;
+    
+    int d=node->data;
+    int lsum=f(node->left);
+    int rsum=f(node->right);
+    node->data=lsum+rsum;
+    
+    return d+node->data;
+}
+void toSumTree(Node *node)
+{
+    f(node);
+}
+
